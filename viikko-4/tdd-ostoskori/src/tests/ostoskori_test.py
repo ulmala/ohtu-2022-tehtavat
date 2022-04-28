4e68fa22cbc6c72b81fb6dee25e3aee3bd17b3ec
Olli Ulmala <ulmala.olli@hotmail.com>
2022-04-28 14:53:59 +0300

    Test kahden tuotteen lisäys lisää kaksi tuotetta

--- viikko-4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko-4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -60,3 +60,11 @@ class TestOstoskori(unittest.TestCase):
         ostos = self.kori.ostokset()[0]
         self.assertEqual(ostos.tuotteen_nimi(), "Maito")
         self.assertEqual(ostos.hinta(), 3)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskori_sisaltaa_kaksi_ostosta(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        voi = Tuote("Voi", 2)
+        self.kori.lisaa_tuote(voi)
+        ostokset = self.kori.ostokset()
+        self.assertEqual(len(ostokset), 2)
