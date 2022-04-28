0113a7ce02cab3b9fd5a3fdff1e1eac00d64cb39
Olli Ulmala <ulmala.olli@hotmail.com>
2022-04-28 14:18:59 +0300

    Test korin hinta on sama kuin tuotteiden hinta

--- viikko-4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ viikko-4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -26,3 +26,10 @@ class TestOstoskori(unittest.TestCase):
         voi = Tuote("Voi", 2)
         self.kori.lisaa_tuote(voi)
         self.assertEqual(self.kori.tavaroita_korissa(), 2)
+
+    def test_kahden_eri_tuotteen_lisaamisen_jalkeen_ostoskorin_hinta_on_sama_kuin_tuotteiden_hintojen_summa(self):
+        maito = Tuote("Maito", 3)
+        self.kori.lisaa_tuote(maito)
+        voi = Tuote("Voi", 2)
+        self.kori.lisaa_tuote(voi)
+        self.assertEqual(self.kori.hinta(), 5)
