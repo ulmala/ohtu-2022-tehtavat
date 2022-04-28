95eaa8b25a779b52e80d0efced63f7a3c9807daf
Olli Ulmala <ulmala.olli@hotmail.com>
2022-04-28 13:56:54 +0300

    Testaa ostoskorin tilaa alussa

---
+++ viikko-4/tdd-ostoskori/src/ostoskori.py
@@ -0,0 +1,40 @@
+from tuote import Tuote
+from ostos import Ostos
+
+class Ostoskori:
+    def __init__(self):
+        self._kori = []
+        # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
+
+    def tavaroita_korissa(self):
+        tavaroita = 0
+        for ostos in self._kori:
+            tavaroita += ostos.lukumaara()
+        return tavaroita
+        # kertoo korissa olevien tavaroiden lukumäärän
+        # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
+        # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
+
+    def hinta(self):
+        hinta = 0
+        for ostos in self._kori:
+            hinta += ostos.hinta()
+        return hinta
+        # kertoo korissa olevien ostosten yhteenlasketun hinnan
+
+    def lisaa_tuote(self, lisattava: Tuote):
+        # lisää tuotteen
+        pass
+
+    def poista_tuote(self, poistettava: Tuote):
+        # poistaa tuotteen
+        pass
+
+    def tyhjenna(self):
+        pass
+        # tyhjentää ostoskorin
+
+    def ostokset(self):
+        pass
+        # palauttaa listan jossa on korissa olevat ostos-oliot
+        # kukin ostos-olio siis kertoo mistä tuotteesta on kyse JA kuinka monta kappaletta kyseistä tuotetta korissa on
